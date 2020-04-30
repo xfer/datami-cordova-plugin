1647bc9c4ebca9307bdd4a9a77405acb1237c7a2
Marcelo Alves <marcelo.alves@me.com>
2020-04-30 15:57:05 -0300

    Cordova@9 support

--- multidex.js
+++ multidex.js
@@ -1,9 +1,10 @@
 module.exports = function(ctx) {
-    var fs = ctx.requireCordovaModule('fs'),
-        path = ctx.requireCordovaModule('path'),
+    var fs = require('fs'),
+        path = require('path'),
         xml = ctx.requireCordovaModule('cordova-common').xmlHelpers;
 
-    var manifestPath = path.join(ctx.opts.projectRoot, 'platforms/android/AndroidManifest.xml');
+    var manifestPath = path.join(ctx.opts.projectRoot, 'platforms/android/app/src/main/AndroidManifest.xml');
+
     var doc = xml.parseElementtreeSync(manifestPath);
     if (doc.getroot().tag !== 'manifest') {
         throw new Error(manifestPath + ' has incorrect root node name (expected "manifest")');
